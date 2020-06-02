0478f418b588124f7fd46b5aeb773927e8e392eb
gboned <gboned@cifpfbmoll.eu>
2020-06-02 20:59:28 +0200

    feat(LlibreOpsBasic): Hacer método carrega devolver libro según isbn

--- src/main/java/org/formacio/setmana1/data/LlibreOpsBasic.java
+++ src/main/java/org/formacio/setmana1/data/LlibreOpsBasic.java
@@ -1,6 +1,10 @@
 package org.formacio.setmana1.data;
 
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 import org.formacio.setmana1.domini.Llibre;
 import org.formacio.setmana1.domini.Recomanacio;
 import org.springframework.stereotype.Repository;
@@ -10,13 +14,19 @@ import org.springframework.stereotype.Repository;
  * operacions de persistencia tal com indiquen les firmes dels metodes
  */
 @Repository
+@Transactional
 public class LlibreOpsBasic {
+	
+	@PersistenceContext
+	private EntityManager em;
 	
 	/**
 	 * Retorna el llibre amb l'ISBN indicat o, si no existeix, llança un LlibreNoExisteixException
 	 */
 	public Llibre carrega (String isbn) throws LlibreNoExisteixException {
-		return null;
+		Llibre llibre = em.find(Llibre.class, isbn);
+		em.persist(llibre);
+		return llibre;
 	}
 	
 	/**
